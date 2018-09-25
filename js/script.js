89f3e2c191329ec06646f94568ea9fff07da2804
Natalia Jędrosz <n.jedrosz@gmail.com>
2018-09-25 17:36:51 +0200

    Update labels on Personal Data and update modal Add links

--- js/script.js
+++ js/script.js
@@ -14,14 +14,59 @@ document.getElementById("hambuger").addEventListener('click', function (e) {
     toggleMenu()
 });
 
-document.getElementById("modalAddLinks").addEventListener('click', function (e) {
-    e.preventDefault();
-    document.getElementsByClassName('index__modal--addLinks')[0].style.display = 'flex';
-});
+// =========================
+// ======= MODALS ==========
+//==========================
+// document.getElementById("modalAddLinks").addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.getElementsByClassName('index__modal--addLinks')[0].style.display = 'flex';
+// });
 
-document.getElementsByClassName("close")[0].addEventListener('click', function (e) {
-    e.preventDefault();
-    document.getElementsByClassName('index__modal--addLinks')[0].style.display = 'none';
-});
+// document.getElementsByClassName("close")[0].addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.getElementsByClassName('index__modal--addLinks')[0].style.display = 'none';
+// });
 
+var showModal = function(event) {
+    
+    event.preventDefault();
+    //chowa modale
+    var modalsHide = document.querySelectorAll('.modal');
+    for(var i = 0; i < modalsHide.length; i++) {
+        modalsHide[i].classList.remove('showM');   
+    }
+    //pokazuje modal-overlay
+    document.querySelector('#modal-overlay').classList.add('showM');
+    console.log(event); //wyciąga wszystkie info o danym obiekcie 
+    //pokazuje modal w zależności od tego, w który button kliknięto
+    document.querySelector(event.target.dataset.hash).classList.add('showM');
 
+};
+
+// stosujemy kod dla wielu linków	
+var modalLinks = document.querySelectorAll('.show-modal');
+for (var i = 0; i < modalLinks.length; i++) {
+      modalLinks[i].addEventListener('click', showModal);
+}
+
+// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
+var hideModal = function(event) {
+      event.preventDefault();
+      document.querySelector('#modal-overlay').classList.remove('showM');
+};
+
+var closeButtons = document.querySelectorAll('.modal .close');
+for (var i = 0; i < closeButtons.length; i++) {
+      closeButtons[i].addEventListener('click', hideModal);
+}
+
+// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
+var modals = document.querySelectorAll('.modal');
+for (var i = 0; i < modals.length; i++) {	  
+modals[i].addEventListener('click', function(event) {
+            event.stopPropagation();
+      });
+}
